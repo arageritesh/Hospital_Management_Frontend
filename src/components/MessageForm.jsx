54e99654b5dc74a19cd1dcb0b3fc56ef7ce594f6
arageritesh <97777090+arageritesh@users.noreply.github.com>
2024-07-13 23:32:09 +0530

    Add files via upload

---
+++ src/components/MessageForm.jsx
@@ -0,0 +1,86 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+
+const MessageForm = () => {
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleMessage = async (e) => {
+    e.preventDefault();
+    try {
+      await axios
+        .post(
+          "https://hopital-management-backend-2.onrender.com/api/v1/message/send",
+          { firstname, lastname, email, phone, message },
+          {
+            withCredentials: true,
+            headers: { "Content-Type": "application/json" },
+          }
+        )
+        .then((res) => {
+          toast.success(res.data.message);
+          setFirstName("");
+          setLastName("");
+          setEmail("");
+          setPhone("");
+          setMessage("");
+        });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  return (
+    <>
+      <div className="container form-component message-form">
+        <h2>Send Us A Message</h2>
+        <form onSubmit={handleMessage}>
+          <div>
+            <input
+              type="text"
+              placeholder="First Name: At least 3 digits"
+              value={firstname}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Last Name: At least 3 digits"
+              value={lastname}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Mobile Number: 11 digits"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+          <textarea
+            rows={7}
+            placeholder="Message: At elast 14 letters"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <div style={{ justifyContent: "center", alignItems: "center" }}>
+            <button type="submit">Send</button>
+          </div>
+        </form>
+        <img src="/Vector.png" alt="vector" />
+      </div>
+    </>
+  );
+};
+
+export default MessageForm;
